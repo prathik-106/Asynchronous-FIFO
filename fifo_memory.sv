0bf303885cbcc79d4482d89bec0b48c6b87d51db
prathik-106 <ee24b045@smail.iitm.ac.in>
2026-06-10 18:29:22 +0530

    Update fifo_memory.sv

--- fifo_memory.sv
+++ fifo_memory.sv
@@ -5,13 +5,13 @@ module fifo_memory #(
     output [DATA_WIDTH-1:0] rdata,
     input [DATA_WIDTH-1:0] wdata,
     input [ADDR_SIZE-1:0] waddr,raddr,
-    input wclk_en,wfull,wclk
+    input wclk_en,wclk
 );
     parameter DEPTH= 1<< ADDR_SIZE;
     reg [DATA_WIDTH-1:0] mem[0:DEPTH-1];
     assign rdata= mem[raddr];
     always@(posedge wclk) begin
-        if(wclk_en && !wfull) begin
+        if(wclk_en) begin
             mem[waddr]<=wdata;
         end
     end
